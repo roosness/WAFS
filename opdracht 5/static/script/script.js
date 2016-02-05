638d16a052c66eebfac3dad4eaab7d15151e13b8
roosness <roos95@live.nl>
2016-02-05 12:13:41 +0100

    Spaties en tabjes gefixt,
    Vergeet niet je gh-pages versie

--- opdracht 5/static/script/script.js
+++ opdracht 5/static/script/script.js
@@ -4,36 +4,33 @@
 
 (function () {
     "use strict";
-    
     var app = {
         init: function () {
             routes.init();
         }
     },
-        
-        routes = {
-            init: function () {
-                window.location.hash = "1";
-                window.addEventListener('hashchange', function (event) {
-                    var route = window.location.hash;
-                    sections.toggle(route);
-                });
-            }
-        },
+    routes = {
+        init: function () {
+            window.location.hash = "1";
+            window.addEventListener('hashchange', function (event) {
+                var route = window.location.hash;
+                sections.toggle(route);
+            });
+        }
+    },
     
-        sections = {
-            toggle: function (route) {
-                var section = document.querySelectorAll('section'),
-                    links = document.querySelectorAll('nav ul li a'),
-                    routeSplit = route.split('#');
-                                
-                Array.prototype.forEach.call(section, function (section) {
-                    section.classList.remove('active');
-                });
-                
-                document.getElementById(routeSplit[1]).classList.add('active');
-            }
-        };
+    sections = {
+        toggle: function (route) {
+            var section = document.querySelectorAll('section'),
+                // deze links is nog ongebruikt en voor nu dus overbodig
+                links = document.querySelectorAll('nav ul li a'),
+                routeSplit = route.split('#');
+            Array.prototype.forEach.call(section, function (section) {
+                section.classList.remove('active');
+            });
+            document.getElementById(routeSplit[1]).classList.add('active');
+        }
+    };
     
     app.init();
     
