5feb52f1b50c252fb164930219287bc32e22c983
EmielZuurbier <emielzuurbier@outlook.com>
2016-02-05 11:40:53 +0100

    Opdracht 5 gh-pages

---
+++ static/script/script.js
@@ -0,0 +1,40 @@
+/*global console, alert, $, window, document*/
+
+// SCRIPT.JS
+
+(function () {
+    "use strict";
+    
+    var app = {
+        init: function () {
+            routes.init();
+        }
+    },
+        
+        routes = {
+            init: function () {
+                window.location.hash = "1";
+                window.addEventListener('hashchange', function (event) {
+                    var route = window.location.hash;
+                    sections.toggle(route);
+                });
+            }
+        },
+    
+        sections = {
+            toggle: function (route) {
+                var section = document.querySelectorAll('section'),
+                    links = document.querySelectorAll('nav ul li a'),
+                    routeSplit = route.split('#');
+                                
+                Array.prototype.forEach.call(section, function (section) {
+                    section.classList.remove('active');
+                });
+                
+                document.getElementById(routeSplit[1]).classList.add('active');
+            }
+        };
+    
+    app.init();
+    
+}());
